46f1e21070f3724ffc721c1d6692a2f98ee951e5
Wojtek B <wojciech.belza@aptiv.com>
2025-09-25 00:30:19 +0200

    feat(server): implement M1 activities CRUD with JSON persistence

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import activitiesRouter from "./routes/activities";
+import activitiesRouter from "./routes/activities.js";
 
 const app = express();
 app.use(cors());
